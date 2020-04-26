cd95bd15b573883d046b9455f3bea6284aeaccda
prks280 <prks280@gmail.com>
2020-04-26 19:34:30 +0530

    Father model added from master branch

--- app1/models.py
+++ app1/models.py
@@ -6,3 +6,11 @@ class Person(models.Model):
 
     def __str__(self):
         return self.name
+
+
+class Father(models.Model):
+    name = models.CharField(max_length=20)
+    nominee = models.ForeignKey(Person, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
